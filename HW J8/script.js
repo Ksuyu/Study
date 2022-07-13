13e7757a7e5b65b961415621f8c3eb6037ce5b42
Ksuyu <98767139+Ksuyu@users.noreply.github.com>
2022-07-13 19:13:16 +0300

    +

--- HW J8/script.js
+++ HW J8/script.js
@@ -5,45 +5,33 @@
 // поле, яке зберігає кількість чорнила в маркері (у відсотках);
 // метод друку (метод приймає рядок і виводить текст відповідним кольором; текст виводиться до тих пір, поки в маркері є чорнило; один не пробіловий символ – це 0,5% чорнила в маркері).
 
-
 class Marker {
-    constructor(color) {
-        this.color = color;
-        this.ink = 100;
-    }
+  constructor(color) {
+    this.color = color;
+    this.ink = 100;
+  }
 
-    print (phrase) {
-        let text = "phrase";
-        let result = " ";
-        for(let i = 0; i < 200; i++) {
-            if (this.ink != 0) {
-                // if (phrase[i] == " ") {
-                //     this.ink += 0.5;
-                //     }
-                this.ink -= 0.5;
-                console.log(this.ink);
-                
-                result += " " + phrase;
-            } 
-            else {
-                console.log("Marker is over");
-                break;
-            }
-            
+  print(phrase) {
+    let result = " ";
+    for (let i = 0; i < 200; i++) {
+      if (this.ink > 0) {
+        if (phrase.includes(" ")) {
+          this.ink += 0.5;
         }
-        // result += " ";
-        return result;
-
+        this.ink -= phrase.length * 0.5;
+        console.log(this.ink); //check
+        result += phrase + " ";
+      } else {
+        console.log("Marker is over");
+        break;
+      }
     }
+    return result;
+  }
 }
-        
 
-
-let marker = new Marker("pink");
-console.log (marker.print("hi there"));
-
-
-
+let marker = new Marker();
+console.log(marker.print("hell boy"));
 
 // !Коло
 // Реалізувати клас, що описує коло. У класі мають бути такі компоненти:
@@ -56,43 +44,37 @@ console.log (marker.print("hi there"));
 // метод, що обчислює довжину кола.
 // Продемонструвати роботу властивостей та методів.
 
+class Circle {
+  constructor(radius) {
+    this._radius = radius;
+  }
 
-// class Circle {
-//     constructor(radius) {
-//         this._radius = radius;
-//     }
+  get radius() {
+    return this._radius;
+  }
 
-//     get radius() {
-//         return this._radius;
-//     }
+  set radius(value) {
+    return (this._radius = value);
+  }
 
-//     set radius(value) {
-//         return this._radius = value;
-//     }
+  get diametr() {
+    return this._radius * 2;
+  }
 
-//     get diametr() {
-//         return this._radius *2;
-//     }
+  area() {
+    return Math.PI * this.radius * this.radius;
+  }
 
-//     area () {
-//         return Math.PI * this.radius * this.radius;
+  length() {
+    return 2 * Math.PI * this.radius;
+  }
+}
 
-//     }
-
-//     length () {
-//         return 2 * Math.PI * this.radius;
-//     }
-// }
-
-// let circle = new Circle(5);
-// console.log(circle.area());
-// console.log(circle.length());
-// console.log(circle.radius);
-// console.log(circle);
-
-
-
-
+let circle = new Circle(5);
+console.log(circle.area());
+console.log(circle.length());
+console.log(circle.radius);
+console.log(circle);
 
 //! Count IP Addresses
 // Implement a function that receives two IPv4 addresses, and returns the number of addresses between them (including the first one, excluding the last one).
@@ -101,6 +83,18 @@ console.log (marker.print("hi there"));
 
 // Examples ipsBetween("10.0.0.0", "10.0.0.50") === 50 ipsBetween("10.0.0.0", "10.0.1.0") === 256 ipsBetween("20.0.0.10", "20.0.1.0") === 246
 
+function findBetween(ip1, ip2) {
+  ip1 = ip1.split('.'); 
+  ip2 = ip2.split('.');
+  let result = 0; 
 
+  for(let i = 0; i < 4; i++) {
+    result += (ip2[i] - ip1[i]) * (256 ** (3-i));
+  }
 
-ББ
+  return result;
+}
+
+console.log(findBetween("10.0.0.0", "10.0.0.50")); //50
+console.log(findBetween("10.0.0.0", "10.0.1.0")); //256
+console.log(findBetween("20.0.0.10", "20.0.1.0")); //246
