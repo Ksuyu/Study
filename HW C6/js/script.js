e47c649a0bed496139816c8d472456b0ca650949
Ksuyu <98767139+Ksuyu@users.noreply.github.com>
2022-07-17 21:37:39 +0300

    _+

--- HW C6/js/script.js
+++ HW C6/js/script.js
@@ -3,7 +3,14 @@ document.addEventListener('DOMContentLoaded', function() {
 const showSlider = new Swiper('.music-player-carousel', {
   loop: true,
   slidesPerView: 3,
-  speed: 1800
+  speed: 1500,
+  centeredSlides: true,
+  navigation: {
+    nextEl: '.music-player-navigation__next',
+    prevEl: '.music-player-navigation__prev'
+
+
+  }
 
 
 //   observer: true,
