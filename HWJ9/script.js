8670337a3a31c92cc75e11e377f304aa955075a6
Ksuyu <98767139+Ksuyu@users.noreply.github.com>
2022-08-01 00:32:34 +0300

    +

---
+++ HWJ9/script.js
@@ -0,0 +1,95 @@
+
+
+const time = document.querySelector('.time');
+
+const minutes = document.querySelector('.mm');
+const seconds = document.querySelector('.ss');
+const milisec = document.querySelector('.ms');
+
+const circle = document.querySelector('.circles-paragraph');
+
+const buttonStart = document.querySelector('.start');
+const buttonStop = document.querySelector('.stop');
+const buttonCircle = document.querySelector('.circle');
+const buttonPause = document.querySelector('.pause');
+
+let counterMM = 0;
+let counterSS = 0;
+let counterMS = 0;
+let timerID;
+
+//Старт
+
+buttonStart.onclick = function () {
+  console.log('btnStart');
+  timerID = 0;
+  timerID = setInterval(function () {
+
+    // milisec
+
+    counterMS++;
+    milisec.innerText = counterMS;
+
+    if (counterMS > 99) {
+      counterSS++;
+      seconds.innerText = "0" + counterSS;
+      counterMS = 0;
+      milisec.innerText = counterMS;
+    }
+
+    // sec
+
+    if (counterSS > 9) {
+      seconds.innerText = counterSS;
+    }
+
+    // minutes
+
+    if(counterSS > 59) {
+      counterMM++;
+      minutes.innerText = "0" + counterMM;
+      counterSS = 0;
+      seconds.innerText = "0" + counterSS;
+    }
+
+    if (counterMM > 9) {
+      minutes.innerText = counterMM;
+    }
+
+  }, 10);
+}
+
+//Стоп
+
+buttonStop.onclick = function () {
+  console.log('btnStop');
+  clearInterval(timerID);
+  milisec.innerText = 0;
+  seconds.innerText = "0" + 0;
+  minutes.innerText = "0" + 0;
+  circle.innerText = "";
+  counterMM = 0;
+  counterSS = 0;
+  counterMS = 0;
+}
+
+//Пауза
+
+buttonPause.onclick = function () {
+  console.log('btnPause');
+  clearInterval(timerID);
+
+}
+
+//Интервал
+
+buttonCircle.onclick = function () {
+  console.log('btnCircle');
+  const newCircle = document.createElement('div');
+  newCircle.innerText = time;
+}
+
+
+
+
+
