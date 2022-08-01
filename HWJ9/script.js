090a457e718a3ce7dd42f7be5961c0fc66bc20e9
Ksuyu <98767139+Ksuyu@users.noreply.github.com>
2022-08-01 19:51:04 +0300

    +

--- HWJ9/script.js
+++ HWJ9/script.js
@@ -1,17 +1,15 @@
+const time = document.querySelector(".time");
 
+const minutes = document.querySelector(".mm");
+const seconds = document.querySelector(".ss");
+const milisec = document.querySelector(".ms");
 
-const time = document.querySelector('.time');
+const circle = document.querySelector(".circles-paragraph");
 
-const minutes = document.querySelector('.mm');
-const seconds = document.querySelector('.ss');
-const milisec = document.querySelector('.ms');
-
-const circle = document.querySelector('.circles-paragraph');
-
-const buttonStart = document.querySelector('.start');
-const buttonStop = document.querySelector('.stop');
-const buttonCircle = document.querySelector('.circle');
-const buttonPause = document.querySelector('.pause');
+const buttonStart = document.querySelector(".start");
+const buttonStop = document.querySelector(".stop");
+const buttonCircle = document.querySelector(".circle");
+const buttonPause = document.querySelector(".pause");
 
 let counterMM = 0;
 let counterSS = 0;
@@ -21,11 +19,10 @@ let timerID;
 //Старт
 
 buttonStart.onclick = function () {
-  console.log('btnStart');
-  
+  console.log("btnStart");
+
   timerID = 0;
   timerID = setInterval(function () {
-
     // milisec
 
     counterMS++;
@@ -46,7 +43,7 @@ buttonStart.onclick = function () {
 
     // minutes
 
-    if(counterSS > 59) {
+    if (counterSS > 59) {
       counterMM++;
       minutes.innerText = "0" + counterMM;
       counterSS = 0;
@@ -56,14 +53,13 @@ buttonStart.onclick = function () {
     if (counterMM > 9) {
       minutes.innerText = counterMM;
     }
-
   }, 10);
-}
+};
 
 //Стоп
 
 buttonStop.onclick = function () {
-  console.log('btnStop');
+  console.log("btnStop");
   clearInterval(timerID);
   milisec.innerText = 0;
   seconds.innerText = "0" + 0;
@@ -72,33 +68,25 @@ buttonStop.onclick = function () {
   counterMM = 0;
   counterSS = 0;
   counterMS = 0;
-}
+};
 
 //Пауза
 
 buttonPause.onclick = function () {
-  console.log('btnPause');
+  console.log("btnPause");
   clearInterval(timerID);
-
-}
+};
 
 //Интервал
 
-
-
 buttonCircle.onclick = function () {
-  console.log('btnCircle');
+  console.log("btnCircle");
 
-  const container = document.querySelector('.circles-paragraph');
-  
-  const newCircle = document.createElement('div');
+  const container = document.querySelector(".circles-paragraph");
+
+  const newCircle = document.createElement("div");
   minutes.innerText = "0" + 0;
   newCircle.innerText = `${minutes.innerText}:${seconds.innerText}.${milisec.innerText}`;
 
   container.append(newCircle);
-}
-
-
-
-
-
+};
