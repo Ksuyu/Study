163d6eaa68287b573308c2c4ad4dd7b7bedf9835
Ksuyu <98767139+Ksuyu@users.noreply.github.com>
2022-08-03 17:20:05 +0300

    +

--- HWJ11/J112/script.js
+++ HWJ11/J112/script.js
@@ -1,68 +1,61 @@
 // !Clicker
-// Напишите приложение, которое помогает пользователю натренироваться кликать левой кнопкой мыши. 
-// При входе появляется кнопка "Начать". По нажатии на кнопку - она пропадает, а вместо нее появляется овальная область в которой нужно кликать мышкой. 
+// Напишите приложение, которое помогает пользователю натренироваться кликать левой кнопкой мыши.
+// При входе появляется кнопка "Начать". По нажатии на кнопку - она пропадает, а вместо нее появляется овальная область в которой нужно кликать мышкой.
 // Справа вверху начинает отсчет таймер с 30 секунд и счетчик кликов пользователя. По окончании этого времени счет кликов не засчитывается. По окончании времени(30 секунд) поле кликов пропадает, появляется таблица статистики и кнопка "Начать". Показывать общее количество кликов и среднюю скорость кликов в минуту.
 
-const btn = document.querySelector('.button-start');
-const clickArea = document.querySelector('.click-area');
-const countdown = document.querySelector('.countdown p');
-const clickCounter = document.querySelector('.click-counter p');
-const table = document.querySelector('.table');
-const totalClicks = document.querySelector('.total-clicks p');
-const averageSpeed = document.querySelector('.average-speed p');
+const btn = document.querySelector(".button-start");
+const clickArea = document.querySelector(".click-area");
+const countdown = document.querySelector(".countdown p");
+const clickCounter = document.querySelector(".click-counter p");
+const table = document.querySelector(".table");
+const totalClicks = document.querySelector(".total-clicks p");
+const averageSpeed = document.querySelector(".average-speed p");
 
-let timerID = 0;
-let t = 30;
+let timerID;
 let click = 0;
-
 
 // кнопка пропадает, появляется поле для кликов
 
-btn.onclick = function() {
-  console.log('Click');
+btn.onclick = function () {
+  if (table.classList.contains("hide")) {
+    btn.classList.add("hide");
+    clickArea.classList.remove("hide");
 
-  btn.classList.add('hide');
-  clickArea.classList.remove('hide');
+    // старт таймера
 
-// старт таймера
-  
-  timerID = setInterval(function(){
-    t--;
-    countdown.innerText = t;
+    let t = 30;
+    timerID = setInterval(function () {
+      t--;
+      countdown.innerText = t;
 
-// остановка таймера
+      // остановка таймера
 
-    if(t <= 0) {
-      clearInterval(t);
-      countdown.innerText = 0;
+      if (t <= 0) {
+        clearInterval(timerID);
+        countdown.innerText = 0;
 
-      btn.classList.remove('hide');
-      clickArea.classList.add('hide');
-      table.classList.remove('hide');
-    }
-
-  }, 1000);
-
-}
+        btn.classList.remove("hide");
+        btn.innerText = "RESET";
+        clickArea.classList.add("hide");
+        table.classList.remove("hide");
+      }
+    }, 1000);
+  } else {
+    table.classList.add("hide");
+    btn.innerText = "START";
+    click = 0;
+    clickCounter.innerText = click;
+  }
+};
 
 // количество кликов
 
-clickArea.onclick = function() {
+clickArea.onclick = function () {
   click++;
   totalClicks.innerText = click;
   clickCounter.innerText = click;
 
-// скорость кликов
+  // скорость кликов
 
-  averageSpeed.innerText = (click / 30).toFixed(2);
-}
-
-
-
-
-
-
-
-
-
-
+  averageSpeed.innerText = click * 2 + "/min";
+};
