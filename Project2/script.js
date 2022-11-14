8d346f946914697e2172a1ac9d1884f5228f32b9
Ksuyu <oksana.chukhalo@gmail.com>
2022-11-14 22:28:46 +0200

    +

--- Project2/script.js
+++ Project2/script.js
@@ -12,6 +12,7 @@
   
   const link = 'https://collectionapi.metmuseum.org/public/collection/v1/objects';
   let OBJS;
+  let ids = [];
   const painting = document.querySelector('.painting');
   const button = document.querySelector('button-random');
 
@@ -19,12 +20,12 @@ function init(){
   ajax(link, (t) => {
       const r = JSON.parse(t);
       OBJS = r.objectIDs;
-
+      // console.log(OBJS);
       getRandomArt();
     });
   }
 
-console.log(OBJS);
+// console.log(OBJS);
 
 
 function getRandom(max){
@@ -32,7 +33,7 @@ function getRandom(max){
 }
 
 function getRandomArt(){
-  const id = OBJS[getRandom(OBJS.length)];
+  let id = OBJS[getRandom(OBJS.length)];
   const url = `${ link }/${ id }`;
   ajax(url, (t) => {
     const r = JSON.parse(t);
@@ -41,10 +42,7 @@ function getRandomArt(){
         console.log(r);
         renderImage(r);
         setArtist(r);
-
       // }
-  
-
   })
 }
 
@@ -53,8 +51,7 @@ function setArtist(n){
     document.querySelector('.artist-name').innerHTML = 'Unknown Artist';
   } else {
   document.querySelector('.artist-name').innerHTML = n.artistDisplayName;
-}
-}
+}}
 
 function renderImage(d){
   painting.src = d.primaryImageSmall;
@@ -63,6 +60,16 @@ function renderImage(d){
 
 }
 
+
+// function carousel(id) { 
+//     for (let i = 0; i < 14; i++) {
+//         if (ids[i] == id) {
+//             return ids;
+//         }
+//     }
+// }
+// console.log(carousel());
+
 document.querySelector('.button-random').addEventListener('click', getRandomArt);
 
 
@@ -70,24 +77,3 @@ document.querySelector('.button-random').addEventListener('click', getRandomArt)
 init();
 
 
-
-
-// function ajaxPOST(url, data, callback){
-//     const aj = new XMLHttpRequest();
-//     aj.onload = () => {
-//         callback(aj.responseText);
-//     }
-
-//     aj.open('POST', url);
-//     aj.setRequestHeader('Content-Type', 'multipart/form-data; boundary=something');
-
-//     aj.send(data);
-// }
-
-// // const urlShort = 'https://cleanuri.com/api/v1/shorten';
-// // const dataShort = 'url=https://github.com/so2niko-students/FE22-1/blob/j19/README.md';
-// // ajaxPOST(urlShort, dataShort, (d) => {
-// //     console.log(d);
-// // });
-
-// // -------
