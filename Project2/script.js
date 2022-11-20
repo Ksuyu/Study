a874df2260ba086967ee9f39d3ce961553180f47
Ksuyu <oksana.chukhalo@gmail.com>
2022-11-20 15:58:11 +0200

    slider

--- Project2/script.js
+++ Project2/script.js
@@ -1,79 +1,72 @@
+const DOM = {
+  btnFind: document.querySelector(".btn-find"),
+  inpArt: document.querySelector(".inp-artist"),
+  slider: document.querySelector(".slide-track"),
+};
 
-  function ajax(url, callback){
-    const aj = new XMLHttpRequest();
-    aj.onload = () => {
-      callback(aj.responseText);
-    }
-    
-    aj.open('GET', url);
-    
-    aj.send();
+const URL = {
+  artist:
+    "https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=",
+  object: "https://collectionapi.metmuseum.org/public/collection/v1/objects/",
+};
+
+const DATA = [];
+
+const MAX_REQUESTS = 50;
+
+DOM.btnFind.addEventListener("click", findArtist);
+
+async function findArtist() {
+  DOM.slider.innerHTML = "";
+  const artistName = DOM.inpArt.value;
+  const artistURL = `${URL.artist}${artistName}`;
+
+  const r = await fetch(artistURL);
+  const d = await r.json();
+
+  console.log(d);
+  const howTimes = d.total / MAX_REQUESTS;
+  for (let i = 0; i <= howTimes; i++) {
+    const from = i * MAX_REQUESTS;
+    const to = from + MAX_REQUESTS;
+    const next50 = d.objectIDs.slice(from, to);
+
+    setTimeout(() => {
+      console.log("REQUEST: ", i);
+      makeMultiRequests(next50, artistName);
+    }, i * 2000);
   }
-  
-  const link = 'https://collectionapi.metmuseum.org/public/collection/v1/objects';
-  let OBJS;
-  let ids = [];
-  const painting = document.querySelector('.painting');
-  const button = document.querySelector('button-random');
-
-function init(){
-  ajax(link, (t) => {
-      const r = JSON.parse(t);
-      OBJS = r.objectIDs;
-      // console.log(OBJS);
-      getRandomArt();
-    });
-  }
-
-// console.log(OBJS);
-
-
-function getRandom(max){
-  return Math.floor(Math.random() * max);
 }
 
-function getRandomArt(){
-  let id = OBJS[getRandom(OBJS.length)];
-  const url = `${ link }/${ id }`;
-  ajax(url, (t) => {
-    const r = JSON.parse(t);
+async function makeMultiRequests(ids, artistName) {
+  console.log(ids);
+  const artistRE = new RegExp(artistName, "ig");
+  const promises = ids.map((id) => {
+    const objectURL = `${URL.object}${id}`;
+    return fetch(objectURL).then((r) => r.json());
+  });
 
-      // if(r.classification = 'Drawings') { НЕ РАБОТАЕТ
-        console.log(r);
-        renderImage(r);
-        setArtist(r);
-      // }
-  })
+  const datas = await Promise.all(promises);
+  const datasFiltered = datas.filter(
+    ({ artistDisplayName, primaryImageSmall }) =>
+      artistRE.test(artistDisplayName) && primaryImageSmall
+  );
+  console.log(datas);
+  console.log(datasFiltered);
+
+  DOM.slider.innerHTML += datasFiltered.reduce(
+    (html, art) => `${html}${render(art)}`,
+    ""
+  );
+  DATA.push(...datasFiltered);
 }
 
-function setArtist(n){
-  if(n.artistDisplayName == '') {
-    document.querySelector('.artist-name').innerHTML = 'Unknown Artist';
-  } else {
-  document.querySelector('.artist-name').innerHTML = n.artistDisplayName;
-}}
-
-function renderImage(d){
-  painting.src = d.primaryImageSmall;
-  painting.setAttribute('alt', d.title);
-  document.querySelector('.painting-year').innerHTML = d.period;
-
+function render({ primaryImageSmall, objectEndDate }) {
+  return `
+  <div class="picture slide">
+    <img class="painting" src="${primaryImageSmall}" alt="...">
+    <p class="details">${objectEndDate}</p>
+    
+  </div>
+    `;
 }
-
-
-// function carousel(id) { 
-//     for (let i = 0; i < 14; i++) {
-//         if (ids[i] == id) {
-//             return ids;
-//         }
-//     }
-// }
-// console.log(carousel());
-
-document.querySelector('.button-random').addEventListener('click', getRandomArt);
-
-
-
-init();
-
-
---
+++ Project2/script_init.js
@@ -0,0 +1,72 @@
+
+  function ajax(url, callback){
+    const aj = new XMLHttpRequest();
+    aj.onload = () => {
+      callback(aj.responseText);
+    }
+    
+    aj.open('GET', url);
+    
+    aj.send();
+  }
+  
+  const link = 'https://collectionapi.metmuseum.org/public/collection/v1/objects';
+  let OBJS;
+  let ids = [];
+  const painting = document.querySelector('.painting');
+  const button = document.querySelector('button-random');
+
+function init(){
+  ajax(link, (t) => {
+      const r = JSON.parse(t);
+      OBJS = r.objectIDs;
+      // console.log(OBJS);
+      getRandomArt();
+    });
+  }
+
+// console.log(OBJS);
+
+
+function getRandom(max){
+  return Math.floor(Math.random() * max);
+}
+
+function getRandomArt(){
+  let id = OBJS[getRandom(OBJS.length)];
+  const url = `${ link }/${ id }`;
+  ajax(url, (t) => {
+    const r = JSON.parse(t);
+
+      // if(r.classification = 'Drawings') { НЕ РАБОТАЕТ
+        console.log(r);
+        renderImage(r);
+        setArtist(r);
+      // }
+  })
+}
+
+function setArtist(n){
+  if(n.artistDisplayName == '') {
+    document.querySelector('.artist-name').innerHTML = 'Unknown Artist';
+  } else {
+  document.querySelector('.artist-name').innerHTML = n.artistDisplayName;
+}}
+
+function renderImage(d){
+  painting.src = d.primaryImageSmall;
+  painting.setAttribute('alt', d.title);
+  document.querySelector('.painting-year').innerHTML = d.period;
+
+}
+
+
+
+
+document.querySelector('.button-random').addEventListener('click', getRandomArt);
+
+
+
+init();
+
+
